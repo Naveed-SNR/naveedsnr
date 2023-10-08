97652d34447c5b68f40a8456cfd6f522d1f871d4
Naveed-SNR <sheikhnaveed3660@gmail.com>
2023-10-08 14:50:03 +0530

    Added dummy content to the navbar

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+import Image from "next/image"
+import logo from "../../../public/vercel.svg";
+export default function Navbar () {
+    return (
+        <>
+            <nav className="navbar items-center">
+                <Image
+                src={logo}
+                width={100}
+                height={54}
+                />
+                <div className="flex absolute font-medium space-x-8 right-0 me-8">
+                    <Link href="/" >About</Link>
+                    <Link href="/" >Projects</Link>
+                    <Link href="/" >Contact</Link>
+                </div>
+            </nav>
+        </>
+    )
+}
