7a05b492cc498d8d54d11689fb0d209b85b21e0a
Naveed-SNR <sheikhnaveed3660@gmail.com>
2023-10-12 11:35:06 +0530

    Improved layout for about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -15,6 +15,16 @@ const explicitTheme = {
   dark: ['#083344', '#164e63', '#155e75', '#0e7490', '#0891b2'],
 };
 
+
+// Shape of `labels` property (default values).
+// All properties are optional.
+const labels = {
+  totalCount: '{{count}} activities in {{year}}',
+  legend: {
+    less: 'Less',
+    more: 'More',
+  },
+};
 export default function About() {
   return (
     <div className="about">
@@ -46,7 +56,7 @@ export default function About() {
       
         <div className="mt-11 flex flex-wrap sm:flex-nowrap gap-4 justify-center shrink align-middle">
           {/* Skills */}
-          <div className="lg:py-11 flex sm:flex-wrap justify-center align-middle max-w-3xl md:max-w-xl sm:max-w-lg bg-cyan-950 dark:bg-gray-900 p-11 rounded-md shrink">
+          <div className="lg:py-11 flex sm:flex-wrap justify-center align-middle max-w-4xl md:max-w-2xl sm:max-w-lg bg-cyan-950 dark:bg-gray-900 p-11 rounded-md shrink">
           <div className="mx-4 text-secondary-100">
                 <h1 className="lg:text-3xl text-2xl">Skills</h1>
                 <div className=" mt-8 text-secondary-200">
@@ -54,19 +64,19 @@ export default function About() {
                       <li className="my-3 ms-4 text-xl">
                           Web Development
                           <ul>
-                              <li className="ms-3 list-circle text-sm">Proficiency in various technologies such as React, Next.js </li>
+                              <li className="ms-3 list-circle lg:text-md text-base">Proficiency in various technologies such as React, Next.js </li>
                           </ul>
                       </li>
                       <li className="my-3 ms-4 text-xl">
                           Digital Media
                           <ul>
-                              <li className="ms-3 list-circle text-sm">Excellent knowledge of technologies like Adboe Suite</li>
+                              <li className="ms-3 list-circle lg:text-md text-base">Excellent knowledge of technologies like Adboe Suite</li>
                           </ul>
                       </li>
-                      <li className="my-3 ms-4 text-xl">
+                      <li className="my-3 ms-3 text-xl">
                           Design
                           <ul>
-                              <li className="ms-3 list-circle text-sm">Great design choices and mastery in design tools like Figma & Adobe XD</li>
+                              <li className="ms-3 list-circle lg:text-md text-base">Great design choices and mastery in design tools like Figma & Adobe XD</li>
                           </ul>
                       </li>
                     </ul>
@@ -74,8 +84,8 @@ export default function About() {
             </div>
           </div>
           {/* Technologies */}
-          <div className="flex flex-wrap justify-center align-middle max-w-3xl md:max-w-xl sm:max-w-lg bg-cyan-950 dark:bg-gray-900 p-11 rounded-md shrink">
-          <div className="">
+          <div className="flex flex-wrap justify-center align-middle max-w-4xl md:max-w-2xl sm:max-w-lg bg-cyan-950 dark:bg-gray-900 p-11 rounded-md shrink">
+          <div className="flex align-middle justify-center flex-col">
                 <h1 className="lg:text-3xl text-2xl">Technologies</h1>
                 <div className="mt-11 flex justify-center align-middle flex-wrap gap-3 max-w-lg text-secondary-200">
                     <div className="flex justify-center align-middle lg:p-8 p-4 rounded-md border border-slate-700">
@@ -109,14 +119,17 @@ export default function About() {
 
 
       {/* GitHub Contributions Graph */}
-      <div className="flex flex-wrap shrink my-11 p-3">
+      <div className="flex flex-wrap shrink my-11 pt-3 p-3">
         <GitHubCalendar
+        className="flex shrink"
           username={`Naveed-SNR`}
           colorScheme="dark"
           theme={explicitTheme}
           fontSize={18.2}
           blockSize={21}
           blockRadius={2}
+          hideColorLegend={true}
+          hideTotalCount={true}
           showWeekdayLabels={true}
           renderBlock={(block, activity) =>
             React.cloneElement(block, {
@@ -126,6 +139,32 @@ export default function About() {
           }
         />
         <ReactTooltip style={{ color: '#00eaff', background: 'black' }} id="react-tooltip" />
+        
+          <div className="flex shrink justify-between items-center w-screen ">
+            <div className="mb-11">
+              <GitHubCalendar
+                username={`Naveed-SNR`}
+                colorScheme="dark"
+                theme={explicitTheme}
+                labels={labels}
+                fontSize={18.2}
+                blockSize={0}
+                hideMonthLabels={true}
+                hideColorLegend={true}
+              />
+            </div>
+            <div className=" shrink flex flex-nowrap gap-1 align-middle">
+              <div>Less</div>
+              <div className="bg-cyan-950 w-4 h-4 rounded-sm"></div>
+              <div className="bg-cyan-900 w-4 h-4 rounded-sm"></div>
+              <div className="bg-cyan-800 w-4 h-4 rounded-sm"></div>
+              <div className="bg-cyan-700 w-4 h-4 rounded-sm"></div>
+              <div className="bg-cyan-600 w-4 h-4 rounded-sm"></div>
+            <div>More</div>
+          </div>
+          </div>
+
+
       </div>
     </div>
   );
