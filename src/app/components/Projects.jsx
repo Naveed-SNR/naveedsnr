b2f72c738675a90f5c5795191f516f90e8667cd6
Naveed-SNR <sheeikhnaveed3660@gmail.com>
2025-01-01 18:23:27 +0530

    Added projects and added links to all projects

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -1,15 +1,17 @@
 "use client"
 import React from 'react'
 import Image from 'next/image'
-import one from "../../../public/images/1.png";
+import cio from "../../../public/images/collegeio.png";
 import fallstreak from "../../../public/images/fallstreak.png";
+import csd from "../../../public/images/codesquad.png";
+import Link from 'next/link'
 import Tilt from 'react-parallax-tilt';
 export default function Projects() {
   return (
     <div id="projects" className="projects py-11 my-30 border border-cyan-300 dark:border-slate-300/11 rounded-lg ">
       <div className="text-center p-11">
         <div className="text-primaryl-100 dark:text-primary-100 text-4xl font-bold">Projects</div>
-        <div className="p-4 text-secondaryl-200 dark:text-secondary-100 text-xl">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam, corporis voluptas?</div>
+        <div className="p-4 text-secondaryl-200 dark:text-secondary-100 text-xl">Here's some of my work.</div>
       </div>
       <div className="mt-4 w-full flex flex-wrap justify-center items-center gap-11">
       {/*cards*/}
@@ -17,7 +19,7 @@ export default function Projects() {
           <div className="w-83 p-3 rounded-lg border-2 border-primaryl-100 dark:border-secondary-100/30 bg-cyan-200 dark:bg-slate-950">
               <div>
                 <Image
-                  src={one}
+                  src={cio}
                   alt="Project Image"
                   objectFit="cover"
                   className="w-full h-full rounded-md border-2 border-primaryl-111 dark:border-secondary-100/21"
@@ -28,8 +30,8 @@ export default function Projects() {
                 <div className="my-8.3 md:my-9.2 font-medium text-md text-secondaryl-100 dark:text-secondary-100">First-person virtual tour using three js and three-mesh-bvh
                 </div>
                 <div className="flex flex-wrap mt-3 gap-3 justify-center w-full">
-                  <button className="bg-primaryl-100 dark:bg-primary-100/83 w-5/12 py-2 px-2 rounded-lg text-sm md:text-md 2xl:text-base font-semibold text-primary-100 dark:text-black button-animation glow-subtle">Demo</button>
-                  <button className="bg-primaryl-100 dark:bg-primary-100/83 w-5/12 py-2 px-2 rounded-lg text-sm md:text-md 2xl:text-base font-semibold text-primary-100 dark:text-black button-animation glow-subtle">Source code</button>
+                  <Link href={"https://college-io.vercel.app"} className="bg-primaryl-100 dark:bg-primary-100/83 w-5/12 py-2 px-2 rounded-lg text-sm md:text-md 2xl:text-base font-semibold text-primary-100 dark:text-black button-animation glow-subtle">Demo</Link>
+                  <Link href={"https://github.com/Naveed-SNR/Potato-FPS"} className="bg-primaryl-100 dark:bg-primary-100/83 w-5/12 py-2 px-2 rounded-lg text-sm md:text-md 2xl:text-base font-semibold text-primary-100 dark:text-black button-animation glow-subtle">Source code</Link>
                 </div>
               </div>
           </div>
@@ -47,11 +49,11 @@ export default function Projects() {
               </div>
               <div className="text-center mt-3 p-4">
                 <div className="text-primaryl-100 dark:text-secondary-200 font-semibold text-2xl">Fall Streak</div>
-                <div className="my-8.3 md:my-9.2 font-medium text-md text-secondaryl-100 dark:text-secondary-100">Travel website built using Next.js, Firebase and Stripe (for payments)
+                <div className="my-8.3 md:my-9.2 font-medium text-md text-secondaryl-100 dark:text-secondary-100">Travel website built using Next.js and Firebase
                 </div>
                 <div className="flex flex-wrap mt-3 gap-3 justify-center w-full">
-                  <button className="bg-primaryl-100 dark:bg-primary-100/83 w-5/12 py-2 px-2 rounded-lg text-sm md:text-md 2xl:text-base font-semibold text-primary-100 dark:text-black button-animation glow-subtle">Demo</button>
-                  <button className="bg-primaryl-100 dark:bg-primary-100/83 w-5/12 py-2 px-2 rounded-lg text-sm md:text-md 2xl:text-base font-semibold text-primary-100 dark:text-black button-animation glow-subtle">Source code</button>
+                  <Link href={"https://www.fallstreak.in"} className="bg-primaryl-100 dark:bg-primary-100/83 w-5/12 py-2 px-2 rounded-lg text-sm md:text-md 2xl:text-base font-semibold text-primary-100 dark:text-black button-animation glow-subtle">Demo</Link>
+                  <Link href={"https://github.com/Naveed-SNR/Fall-Streak"} className="bg-primaryl-100 dark:bg-primary-100/83 w-5/12 py-2 px-2 rounded-lg text-sm md:text-md 2xl:text-base font-semibold text-primary-100 dark:text-black button-animation glow-subtle">Source code</Link>
                 </div>
               </div>
           </div>
@@ -61,7 +63,7 @@ export default function Projects() {
           <div className="w-83 p-3 rounded-lg border-2 border-primaryl-100 dark:border-secondary-100/30 bg-cyan-200 dark:bg-slate-950">
               <div>
                 <Image
-                  src={one}
+                  src={csd}
                   alt="Project Image"
                   objectFit="cover"
                   className="w-full h-full rounded-md border-2 border-primaryl-111 dark:border-secondary-100/21"
@@ -69,11 +71,11 @@ export default function Projects() {
               </div>
               <div className="text-center mt-3 p-4">
                 <div className="text-primaryl-100 dark:text-secondary-200 font-semibold text-2xl">College IO</div>
-                <div className="my-8.3 md:my-9.2 font-medium text-md text-secondaryl-100 dark:text-secondary-100">First-person virtual tour using three js and three-mesh-bvh
+                <div className="my-8.3 md:my-9.2 font-medium text-md text-secondaryl-100 dark:text-secondary-100">Website for Codesquad, a club founded by students of CUK
                 </div>
                 <div className="flex flex-wrap mt-3 gap-3 justify-center w-full">
-                  <button className="bg-primaryl-100 dark:bg-primary-100/83 w-5/12 py-2 px-2 rounded-lg text-sm md:text-md 2xl:text-base font-semibold text-primary-100 dark:text-black button-animation glow-subtle">Demo</button>
-                  <button className="bg-primaryl-100 dark:bg-primary-100/83 w-5/12 py-2 px-2 rounded-lg text-sm md:text-md 2xl:text-base font-semibold text-primary-100 dark:text-black button-animation glow-subtle">Source code</button>
+                  <Link href={"https://codesquad-cuk.vercel.app"} className="bg-primaryl-100 dark:bg-primary-100/83 w-5/12 py-2 px-2 rounded-lg text-sm md:text-md 2xl:text-base font-semibold text-primary-100 dark:text-black button-animation glow-subtle">Demo</Link>
+                  <Link href={"https://github.com/devdaim6/department-of-it"} className="bg-primaryl-100 dark:bg-primary-100/83 w-5/12 py-2 px-2 rounded-lg text-sm md:text-md 2xl:text-base font-semibold text-primary-100 dark:text-black button-animation glow-subtle">Source code</Link>
                 </div>
               </div>
           </div>
